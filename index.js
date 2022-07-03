6d09ffd86022041e1fabee19a7bd1fe53f90270d
cheetoah <90515754+cheetoah@users.noreply.github.com>
2022-07-03 20:46:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //chattriggers module (chattriggers.com) --docs on how to use it on their site
 
+/// <reference types="../CTAutocomplete" />
+/// <reference lib="es2015" />
+
 register("command", (...args) => {
     let qu = replaceAll(args.join(''), "x", "*")
     let answer
     try {
         answer = eval( replaceAll(args.join(''), ")(", ")*("))
         ChatLib.chat(answer)
-        ChatLib.say("/ac Solve " + replaceAll(qu, "*", "x") +" first for nothing but ego boost!")
+        ChatLib.say("/ac :star: Solve " + replaceAll(qu, "*", "x") +" first for cool! :star:")
     } catch (e) {
         ChatLib.chat("§c" + e)
         return
@@ -16,13 +19,22 @@ register("command", (...args) => {
         if (msg == answer){
             let name =  chatter.replace(/&./g, "").replace(/^\[[^\]]+\] /, "").replace(/ \[[^\]]+\]$/, ""); //thanks Semisol
             let time = Date.now() - startTime
-            setTimeout(function(){ChatLib.say("/ac " + name + " answered correctly first, after " + time + "ms :yes:")}, 250)
+            setTimeout(function(){ChatLib.say("/ac :yes: " + name + " answered correctly after " + Math.floor((time / 1000) * 100) / 100 + " seconds!")}, 250)
             cr.unregister()
         }
     }).setChatCriteria("&r${chatter}: ${msg}&r")
 }).setName("respond")
 
-//definitely a better way to do this
 function replaceAll(string, search, replace) {
     return string.split(search).join(replace);
 }
+/*
+register("chat", (question) => {
+    let question = replaceAll(question, "x", "*")
+    let answer = eval(question)
+    ChatLib.chat("§3[§9Quick Math Solver§3] §b" + question + " = " + answer)
+    ChatLib.chat("§7Sending in 10ms")
+    setTimeout(function(){ChatLib.say("/ac " + answer)}, 10)
+}).setChatCriteria("&r&d&lQUICK MATHS! &r&7Solve: &r&e${question}&r")
+*/ //uncomment for auto admin quick maths solver
+
